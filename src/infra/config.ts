dacd848059a2a2f669593505d212564daa6cee5b
badmagick329 <uzairfarooqi@gmail.com>
2025-09-21 22:18:28 +0100

    refactor

--- src/infra/config.ts
+++ src/infra/config.ts
@@ -3,15 +3,11 @@ import conf from "../../config.toml";
 type ConfigType = {
   src: string;
   dst: string;
-  sampleCmd: string;
-  sampleInput: string;
   cmdsInputDir: string;
 };
 
 export const config: ConfigType = {
   src: conf.src,
   dst: conf.dst,
-  sampleCmd: conf.sampleCmd,
-  sampleInput: conf.sampleInput,
   cmdsInputDir: conf.cmdsInputDir,
 };
